aa5638b42f4747c629472f96b69f47762d94bfc3
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-21 21:42:11 +0200

    add js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
+import './js/products-api';
+import './js/mobile-menu';
+import './js/footer';
 import './js/cart';
 import './js/discount';
+import './js/header';
+import './js/hiro';
+import './js/popular';
+
+
