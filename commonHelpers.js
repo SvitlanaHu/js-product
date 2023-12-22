1c686431161de5cdd18edb8ef28f9fa7ee79ae95
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-22 13:04:16 +0000

    Deploying to gh-pages from @ SvitlanaHu/js-product@d9969ff1e83496888dbe5d96b006a382915df9d2 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,27 +1,27 @@
-import{a}from"./assets/vendor-a61d8330.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();const l="https://food-boutique.b.goit.study/api/products";function u(s,t){return a.get(`${l}`,{params:{page:s,limit:t}})}const d=document.querySelector(".main-container");async function p(){let s=1,t=6;try{const{data:r}=await u(s,t);console.log(r);const{results:i}=r;console.log(i),n(i),d.innerHTML+=n(i)}catch(r){console.error("Error fetching products",r)}}p();function n(s){return`<div class="card-container">${s.map(t=>{const r=t.category.split("_").join(" ");return`
-            <div class="photo-card">
-                <a class="product-modal" href="МОДАЛЬНЕ ВІКНО">
-                    <div class="img-container">
-                        <img class="product-image" src="${t.img}" alt="${t.name} photo" width=140 height=140 loading="lazy" />
+import{a as l}from"./assets/vendor-a61d8330.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();const a="https://food-boutique.b.goit.study/api/products";function u(o,t){return l.get(`${a}`,{params:{page:o,limit:t}})}const d=document.querySelector(".main-container");async function p(){let o=1,t=window.innerWidth>=768?8:6;try{const{data:i}=await u(o,t);console.log(i);const{results:r}=i;console.log(r),c(r),d.innerHTML+=c(r)}catch(i){console.error("Error fetching products",i)}}p();function c(o){return`<div class="card-container-list">${o.map(t=>{const i=t.category.split("_").join(" ");return`
+            <div class="photo-card-list">
+                <a class="product-modal-list" href="МОДАЛЬНЕ ВІКНО">
+                    <div class="img-container-list">
+                        <img class="product-image-list" src="${t.img}" alt="${t.name} photo" width=140 height=140 loading="lazy" />
                     </div>
-                    <div class="product-info">
-                        <h2 class="product-name">${t.name}</h2>
-                        <div class='product-atributes'>
-                            <p class="atributes-info">
-                                <b class="atributes">Category:</b> ${r}
+                    <div class="product-info-list">
+                        <h2 class="product-name-list">${t.name}</h2>
+                        <div class='product-atributes-list'>
+                            <p class="atributes-info-list">
+                                <b class="atributes-list">Category:</b> ${i}
                             </p>
-                            <p class="atributes-info">
-                                <b class="atributes">Size:</b> ${t.size}
+                            <p class="atributes-info-list">
+                                <b class="atributes-list">Size:</b> ${t.size}
                             </p>
-                            <p class="atributes-info">
-                                <b class="atributes">Popularity:</b> ${t.popularity}
+                            <p class="atributes-info-list">
+                                <b class="atributes-list">Popularity:</b> ${t.popularity}
                             </p>
                         </div>
                         
-                        <div class="price-and-btn">
-                            <h2 class="product-price">$${t.price}</h2>
-                            <button class='cart-btn' type="button">          
-                                <svg class='list-cart-svg' width="18" height="18" >
+                        <div class="price-and-btn-list">
+                            <h2 class="product-price-list">$${t.price}</h2>
+                            <button class='cart-btn-list' type="button">          
+                                <svg class='list-cart-svg-list' width="18" height="18" >
                                     <use href="./img/icone/symbol-defs.svg#icon-heroicons-solid_shopping-cart-18x18"></use>
                                 </svg>
                             </button>
