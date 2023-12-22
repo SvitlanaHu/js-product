79fdaefefdb0cb860475d48196a5bdcc043b89c9
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-22 21:16:04 +0000

    Deploying to gh-pages from @ SvitlanaHu/js-product@35e1c988ef6f336c6f66ab6b7face5b5e37f26fd 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{a as l}from"./assets/vendor-a61d8330.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();const a="/js-product/assets/symbol-defs-bacee5fb.svg",u="https://food-boutique.b.goit.study/api/products";function d(o,t){return l.get(`${u}`,{params:{page:o,limit:t}})}const p=document.querySelector(".main-container");async function f(){let o=1,t;window.innerWidth>=1440?t=9:window.innerWidth>=768?t=8:t=6;try{const{data:i}=await d(o,t);console.log(i);const{results:r}=i;console.log(r),c(r),p.innerHTML+=c(r)}catch(i){console.error("Error fetching products",i)}}f();function c(o){return`<ul class="card-container-list">${o.map(t=>{const i=t.category.split("_").join(" ");return`
+import{a as d}from"./assets/vendor-a61d8330.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(e){if(e.ep)return;e.ep=!0;const o=s(e);fetch(e.href,o)}})();const u="/js-product/assets/symbol-defs-bacee5fb.svg",p="https://food-boutique.b.goit.study/api/products";function f(i,t){return d.get(`${p}`,{params:{page:i,limit:t}})}const h=document.querySelector(".main-container");async function g(){let i=1,t;window.innerWidth>=1440?t=9:window.innerWidth>=768?t=8:t=6;try{const{data:s}=await f(i,t);console.log(s);const{results:r}=s;console.log(r),l(r),h.innerHTML+=l(r)}catch(s){console.error("Error fetching products",s)}}g();function l(i){return`<ul class="card-container-list">${i.map(t=>{const s=t.category.split("_").join(" ");return`
             <li class="photo-card-list">
                 <a class="product-modal-list" href="МОДАЛЬНЕ ВІКНО"> </a>
                     <div class="img-container-list">
@@ -8,7 +8,7 @@ import{a as l}from"./assets/vendor-a61d8330.js";(function(){const t=document.cre
                         <h2 class="product-name-list">${t.name}</h2>
                         <div class='product-atributes-list'>
                             <p class="atributes-info-list">
-                                <b class="atributes-list">Category:</b> ${i}
+                                <b class="atributes-list">Category:</b> ${s}
                             </p>
                             <p class="atributes-info-list">
                                 <b class="atributes-list">Size:</b> ${t.size}
@@ -22,7 +22,7 @@ import{a as l}from"./assets/vendor-a61d8330.js";(function(){const t=document.cre
                             <h2 class="product-price-list">$${t.price}</h2>
                             <button class='cart-btn-list' type="button">          
                                 <svg class="list-cart-svg-list" width="18" height="18">
-                                    <use href="${a}#icon-heroicons-solid_shopping-cart-18x18">
+                                    <use href="${u}#icon-heroicons-solid_shopping-cart-18x18">
                                     </use>
                                 </svg>
                             </button>
@@ -30,5 +30,5 @@ import{a as l}from"./assets/vendor-a61d8330.js";(function(){const t=document.cre
                     </div>
                
             </li>
-        `}).join("")}</ul>`}
+        `}).join("")}</ul>`}window.onscroll=()=>c();window.addEventListener("scroll",a(c,250)),window.addEventListener("resize",a(c,250));async function c(){const i=document.querySelector("body"),t=document.body.offsetHeight,s=window.innerHeight,r=window.scrollY,e=t-s/4;r+s>e?i.classList.add("body--no-transparency"):i.classList.remove("body--no-transparency")}function a(i,t){let s=null;return function(...e){s||(s=setTimeout(()=>{i(...e),clearTimeout(s),s=null},t))}}
 //# sourceMappingURL=commonHelpers.js.map
