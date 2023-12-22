ccbcdaed6b71b22190603a8608af5869f7c29433
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-12-23 01:35:36 +0200

    moved btn paginations.js in productslist.js

--- src/js/buttons-paginations.js
+++ src/js/buttons-paginations.js
@@ -1,11 +1,11 @@
-import 'tui-pagination/dist/tui-pagination.css';
-import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
+// import Pagination from 'tui-pagination';
 
-const paginationContainer = document.getElementById('pagination');
+// const paginationContainer = document.getElementById('pagination');
 
-export const pagination = new Pagination(paginationContainer, {
-  totalItems: 500,
-  itemsPerPage: 10,
-  visiblePages: 4,
-  centerAlign: true,
-});
+// export const pagination = new Pagination(paginationContainer, {
+//   totalItems: 500,
+//   itemsPerPage: 10,
+//   visiblePages: 4,
+//   centerAlign: true,
+// });
--- src/js/products-list.js
+++ src/js/products-list.js
@@ -1,46 +1,63 @@
-import icons from '../img/icone/symbol-defs.svg'
+import icons from '../img/icone/symbol-defs.svg';
 import { getProducts } from './products-api';
+import 'tui-pagination/dist/tui-pagination.css';
+import Pagination from 'tui-pagination';
 
-
-const mainProductCard = document.querySelector('.main-container')
+const mainProductCard = document.querySelector('.main-container');
 // const formEl = document.querySelector('.search-form')
 
 async function renderProducts() {
-    let page = 1;
-    let limit;
+  let page = 1;
+  let limit;
 
-    if (window.innerWidth >= 1440) {
-      limit = 9;
-    } else if (window.innerWidth >= 768) {
-      limit = 8;
-    } else {
-      limit = 6;
-    } 
+  if (window.innerWidth >= 1440) {
+    limit = 9;
+  } else if (window.innerWidth >= 768) {
+    limit = 8;
+  } else {
+    limit = 6;
+  }
 
-    try {
-      const { data } = await getProducts(page, limit);
-      console.log(data);
-     const { results } = data;
-     console.log(results);
-     createMarkup(results)
+  try {
+    const { data } = await getProducts(page, limit);
+    console.log(data);
+      const { perPage, totalPages, results } = data;
+      const totalItems = perPage * totalPages;
+    console.log(results);
+    createMarkup(results);
 
-     
-    mainProductCard.innerHTML += createMarkup(results);
-    
-} catch (error) {
-        console.error('Error fetching products', error);
-      }
-    }
-  
-    // Call the renderProducts function to automatically render the first page on page load
-    renderProducts();
-    
-   function createMarkup(arr) {
-    return `<ul class="card-container-list">${arr.map(item => {
-        const categoryWithoutUnderscore = item.category.split('_').join(' ');
-        return `
-            <li class="photo-card-list">
-                <a class="product-modal-list" href="МОДАЛЬНЕ ВІКНО"> </a>
+    mainProductCard.innerHTML =
+      createMarkup(results) +
+      `
+  <div id="tui-pagination-container" class="tui-pagination"></div>
+</div>
+`;
+
+    const container = document.getElementById('tui-pagination-container');
+    const pagination = new Pagination(container, {
+      totalItems: totalItems,
+      itemsPerPage: limit,
+      visiblePages: 4,
+      centerAlign: true,
+    });
+    pagination.on('change', page => {
+      renderProducts(page);
+    });
+  } catch (error) {
+    console.error('Error fetching products', error);
+  }
+}
+
+// Call the renderProducts function to automatically render the first page on page load
+renderProducts();
+
+function createMarkup(arr) {
+  return `<div class="card-container-list">${arr
+    .map(item => {
+      const categoryWithoutUnderscore = item.category.split('_').join(' ');
+      return `
+            <div class="photo-card-list">
+                <a class="product-modal-list" href="МОДАЛЬНЕ ВІКНО">
                     <div class="img-container-list">
                         <img class="product-image-list" src="${item.img}" alt="${item.name} photo" width=140 height=140 loading="lazy" />
                     </div>
@@ -61,15 +78,17 @@ async function renderProducts() {
                         <div class="price-and-btn-list">
                             <h2 class="product-price-list">$${item.price}</h2>
                             <button class='cart-btn-list' type="button">          
-                                <svg class="list-cart-svg-list" width="18" height="18">
-                                    <use href="${icons}#icon-heroicons-solid_shopping-cart-18x18">
-                                    </use>
+                                <svg class='list-cart-svg-list' width="18" height="18" >
+                                    <use href="../img/icone/symbol-defs.svg#icon-heroicons-solid_shopping-cart-18x18"></use>
                                 </svg>
                             </button>
                         </div>
                     </div>
-               
-            </li>
+                </a>
+            </div>
         `;
-    }).join('')}</ul>`;
+    })
+    .join('')}
+
+	 `;
 }
