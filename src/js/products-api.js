304c3638857c18e0643403a1afa6ad3da6bacdec
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-22 13:15:49 +0200

    update index.html

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,60 +1,55 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const BASE_URL = 'https://food-boutique.b.goit.study/api';
+const BASE_URL = 'https://food-boutique.b.goit.study/api-docs';
 
-// export default class FoodBoutiqueApiService {
-//     constructor() {
-//         this.searchQuery = '';
-//         this.page = 1;
-//         this.PER_PAGE = 9;
-//     }
-//     async fetchPictures() {
-//         const axiosOptions = {
-//             method: 'get',
-//             url: '${BASE_URL}',
-//             params: {
-//                 key: KEY,
-//                 q: `${this.searchQuery}`,
-//                 image_type: 'photo',
-//                 orientation: 'horizontal',
-//                 safesearch: true,
-//                 page: `${this.page}`,
-//                 per_page: `${this.PER_PAGE}`,
-//             },
-//         };
-//         try {
-//             const response = await axios(axiosOptions);
+export default class FoodBoutiqueApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        this.PER_PAGE = 9;
+    }
+    async fetchProducts() {
+        const axiosOptions = {
+            method: 'get',
+            url: '${BASE_URL}',
+            params: {
+                q: `${this.searchQuery}`,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page: `${this.page}`,
+                per_page: `${this.PER_PAGE}`,
+            },
+        };
+        try {
+            const response = await axios(axiosOptions);
+            const data = response.data;
 
-//             const data = response.data;
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-//             // this.incrementPage();
-//             return data;
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     }
+    //     incrementPage() {
+    //         this.page += 1;
+    //     }
 
-//     incrementPage() {
-//         this.page += 1;
-//     }
+    //     resetPage() {
+    //         this.page = 1;
+    //     }
 
-//     resetPage() {
-//         this.page = 1;
-//     }
+    //     resetEndOfHits() {
+    //         this.endOfHits = false;
+    //     }
 
-//     resetEndOfHits() {
-//         this.endOfHits = false;
-//     }
+    //     get query() {
+    //         return this.searchQuery;
+    //     }
 
-//     get query() {
-//         return this.searchQuery;
-//     }
-
-//     set query(newQuery) {
-//         this.searchQuery = newQuery;
-//     }
-// }
-
-// "page": 1,
-//     "perPage": 3,
-//         "totalPages": 4,
+    //     set query(newQuery) {
+    //         this.searchQuery = newQuery;
+    //     }
+}
+const foodBoutiqueApiService = new FoodBoutiqueApiService();
+console.log("query: ", foodBoutiqueApiService.query);
