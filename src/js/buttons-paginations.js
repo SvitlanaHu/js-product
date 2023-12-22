c2e907154fcda3879ab17cdefa747b0e29e4065b
Serhii-Kysil <serik.kisel000@gmail.com>
2023-12-23 00:25:48 +0200

    add export pag

--- src/js/buttons-paginations.js
+++ src/js/buttons-paginations.js
@@ -3,9 +3,9 @@ import Pagination from 'tui-pagination';
 
 const paginationContainer = document.getElementById('pagination');
 
-const pagination = new Pagination(paginationContainer, {
-  totalItems: totalItems,
-  itemsPerPage: perPage,
+export const pagination = new Pagination(paginationContainer, {
+  totalItems: 500,
+  itemsPerPage: 10,
   visiblePages: 4,
   centerAlign: true,
 });
