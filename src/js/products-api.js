09aed69ef925102a4e6c5f831d79f9dcd424bfe3
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-22 14:50:39 +0200

    update products-api.js

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,55 +1,25 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://food-boutique.b.goit.study/api-docs';
+const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
 
-export default class FoodBoutiqueApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-        this.PER_PAGE = 9;
+export async function fetchProducts(page, limit) {
+    const axiosOptions = {
+        method: 'get',
+        url: '${BASE_URL}',
+        params: {
+            page: page,
+            limit: limit,
+        },
+    };
+    try {
+        const response = await axios(axiosOptions);
+        const data = response.data;
+        console.log('response:', response);
+        console.log('data:', data);
+        return data;
+
+    } catch (error) {
+        console.error(error);
     }
-    async fetchProducts() {
-        const axiosOptions = {
-            method: 'get',
-            url: '${BASE_URL}',
-            params: {
-                q: `${this.searchQuery}`,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: true,
-                page: `${this.page}`,
-                per_page: `${this.PER_PAGE}`,
-            },
-        };
-        try {
-            const response = await axios(axiosOptions);
-            const data = response.data;
-
-            return data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    //     incrementPage() {
-    //         this.page += 1;
-    //     }
-
-    //     resetPage() {
-    //         this.page = 1;
-    //     }
-
-    //     resetEndOfHits() {
-    //         this.endOfHits = false;
-    //     }
-
-    //     get query() {
-    //         return this.searchQuery;
-    //     }
-
-    //     set query(newQuery) {
-    //         this.searchQuery = newQuery;
-    //     }
 }
-const foodBoutiqueApiService = new FoodBoutiqueApiService();
-console.log("query: ", foodBoutiqueApiService.query);
+
