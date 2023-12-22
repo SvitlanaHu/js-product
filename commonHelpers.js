412e75ff4db7a83f092905a5095882595a68b33c
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-22 19:33:20 +0000

    Deploying to gh-pages from @ SvitlanaHu/js-product@a38fc387653b3969d869bbdd635c9a42c8b0bcdf 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,6 @@
-import{a as l}from"./assets/vendor-a61d8330.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();const a="https://food-boutique.b.goit.study/api/products";function d(o,t){return l.get(`${a}`,{params:{page:o,limit:t}})}const u=document.querySelector(".main-container");async function p(){let o=1,t;window.innerWidth>=1440?t=9:window.innerWidth>=768?t=8:t=6;try{const{data:i}=await d(o,t);console.log(i);const{results:r}=i;console.log(r),c(r),u.innerHTML+=c(r)}catch(i){console.error("Error fetching products",i)}}p();function c(o){return`<div class="card-container-list">${o.map(t=>{const i=t.category.split("_").join(" ");return`
-            <div class="photo-card-list">
-                <a class="product-modal-list" href="МОДАЛЬНЕ ВІКНО">
+import{a as l}from"./assets/vendor-a61d8330.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();const a="/js-product/assets/symbol-defs-bacee5fb.svg",u="https://food-boutique.b.goit.study/api/products";function d(o,t){return l.get(`${u}`,{params:{page:o,limit:t}})}const p=document.querySelector(".main-container");async function f(){let o=1,t;window.innerWidth>=1440?t=9:window.innerWidth>=768?t=8:t=6;try{const{data:i}=await d(o,t);console.log(i);const{results:r}=i;console.log(r),c(r),p.innerHTML+=c(r)}catch(i){console.error("Error fetching products",i)}}f();function c(o){return`<ul class="card-container-list">${o.map(t=>{const i=t.category.split("_").join(" ");return`
+            <li class="photo-card-list">
+                <a class="product-modal-list" href="МОДАЛЬНЕ ВІКНО"> </a>
                     <div class="img-container-list">
                         <img class="product-image-list" src="${t.img}" alt="${t.name} photo" width=140 height=140 loading="lazy" />
                     </div>
@@ -22,13 +22,13 @@ import{a as l}from"./assets/vendor-a61d8330.js";(function(){const t=document.cre
                             <h2 class="product-price-list">$${t.price}</h2>
                             <button class='cart-btn-list' type="button">          
                                 <svg class="list-cart-svg-list" width="18" height="18">
-                                    <use href="./img/icone/symbol-defs.svg#icon-heroicons-solid_shopping-cart-18x18">
+                                    <use href="${a}#icon-heroicons-solid_shopping-cart-18x18">
                                     </use>
                                 </svg>
                             </button>
                         </div>
                     </div>
-                </a>
-            </div>
-        `}).join("")}</div>`}
+               
+            </li>
+        `}).join("")}</ul>`}
 //# sourceMappingURL=commonHelpers.js.map
