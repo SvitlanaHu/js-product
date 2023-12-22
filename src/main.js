b0ae1f9406ad2f88bbe4a7ec5c915a92eb756df6
jaycikey <138725941+jaycikey@users.noreply.github.com>
2023-12-22 06:27:12 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import './js/products-api';
-import './js/mobile-menu';
+import './js/buttons-paginations';
 import './js/footer';
-import './js/cart';
+import './js/home-page';
 import './js/discount';
 import './js/header';
-import './js/hiro';
+import './js/local-storage';
+import './js/hero';
 import './js/popular';
+import "./js/filters";
+import './js/products-list';
+import './js/modal-window';
+import './js/cart-page';
