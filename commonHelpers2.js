6498de478ccd25160c76777333ed3b2fd1172de0
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-23 09:43:29 +0000

    Deploying to gh-pages from @ SvitlanaHu/js-product@20b2ac10f8c2ee691365a36afd8c657a7453342f 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,8 +1,8 @@
-import"./assets/styles-8194e3e2.js";import{a as g,P as h}from"./assets/vendor-b592f4c5.js";const b="https://food-boutique.b.goit.study/api/products";function m(s,t){return g.get(`${b}`,{params:{page:s,limit:t}})}const f=document.querySelector(".main-container");async function l(){let s=1,t;window.innerWidth>=1440?t=9:window.innerWidth>=768?t=8:t=6;try{const{data:i}=await m(s,t);console.log(i);const{perPage:n,totalPages:o,results:e}=i,d=n*o;console.log(e),r(e),f.innerHTML=r(e)+`
+import"./assets/styles-8194e3e2.js";import{a as g,P as h}from"./assets/vendor-b592f4c5.js";const b="https://food-boutique.b.goit.study/api/products";function m(i,t){return g.get(`${b}`,{params:{page:i,limit:t}})}const f="/js-product/assets/symbol-defs-a3120aea.svg",y=document.querySelector(".main-box");async function l(){let i=1,t;window.innerWidth>=1440?t=9:window.innerWidth>=768?t=8:t=6;try{const{data:s}=await m(i,t);console.log(s);const{perPage:n,totalPages:o,results:e}=s,d=n*o;console.log(e),r(e),y.innerHTML=r(e)+`
   <div id="tui-pagination-container" class="tui-pagination"></div>
 </div>
-`;const u=document.getElementById("tui-pagination-container");new h(u,{totalItems:d,itemsPerPage:t,visiblePages:4,centerAlign:!0}).on("change",p=>{l(p)})}catch(i){console.error("Error fetching products",i)}}l();function r(s){return`<div class="card-container-list">${s.map(t=>{const i=t.category.split("_").join(" ");return`
-            <div class="photo-card-list">
+`;const u=document.getElementById("tui-pagination-container");new h(u,{totalItems:d,itemsPerPage:t,visiblePages:4,centerAlign:!0}).on("change",p=>{l(p)})}catch(s){console.error("Error fetching products",s)}}l();function r(i){return`<ul class="card-container-list">${i.map(t=>{const s=t.category.split("_").join(" ");return`
+            <li class="photo-card-list">
                 <a class="product-modal-list" href="МОДАЛЬНЕ ВІКНО">
                     <div class="img-container-list">
                         <img class="product-image-list" src="${t.img}" alt="${t.name} photo" width=140 height=140 loading="lazy" />
@@ -11,7 +11,7 @@ import"./assets/styles-8194e3e2.js";import{a as g,P as h}from"./assets/vendor-b5
                         <h2 class="product-name-list">${t.name}</h2>
                         <div class='product-atributes-list'>
                             <p class="atributes-info-list">
-                                <b class="atributes-list">Category:</b> ${i}
+                                <b class="atributes-list">Category:</b> ${s}
                             </p>
                             <p class="atributes-info-list">
                                 <b class="atributes-list">Size:</b> ${t.size}
@@ -24,15 +24,16 @@ import"./assets/styles-8194e3e2.js";import{a as g,P as h}from"./assets/vendor-b5
                         <div class="price-and-btn-list">
                             <h2 class="product-price-list">$${t.price}</h2>
                             <button class='cart-btn-list' type="button">          
-                                <svg class='list-cart-svg-list' width="18" height="18" >
-                                    <use href="../img/icone/symbol-defs.svg#icon-heroicons-solid_shopping-cart-18x18"></use>
+                                <svg class="list-cart-svg-list" width="18" height="18">
+                                    <use href="${f}#icon-heroicons-solid_shopping-cart-18x18">
+                                    </use>
                                 </svg>
                             </button>
                         </div>
                     </div>
                 </a>
-            </div>
+            </li>
         `}).join("")}
 
-	 `}window.onscroll=()=>a();window.addEventListener("scroll",c(a,250)),window.addEventListener("resize",c(a,250));async function a(){const s=document.querySelector("body"),t=document.body.offsetHeight,i=window.innerHeight,n=window.scrollY,o=t-i/4;n+i>o?s.classList.add("body--no-transparency"):s.classList.remove("body--no-transparency")}function c(s,t){let i=null;return function(...o){i||(i=setTimeout(()=>{s(...o),clearTimeout(i),i=null},t))}}
+	 `}window.onscroll=()=>a();window.addEventListener("scroll",c(a,250)),window.addEventListener("resize",c(a,250));async function a(){const i=document.querySelector("body"),t=document.body.offsetHeight,s=window.innerHeight,n=window.scrollY,o=t-s/4;n+s>o?i.classList.add("body--no-transparency"):i.classList.remove("body--no-transparency")}function c(i,t){let s=null;return function(...o){s||(s=setTimeout(()=>{i(...o),clearTimeout(s),s=null},t))}}
 //# sourceMappingURL=commonHelpers2.js.map
