6ba07292787100f9259140f3c7cc89d9e2cc6cec
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-24 10:00:33 +0000

    Deploying to gh-pages from @ SvitlanaHu/js-product@acd297b730deaa9a4adaf848c6bfc6321b6fde72 🚀

--- assets/products-list-8638d7ff.js
+++ assets/products-list-8638d7ff.js
@@ -38,4 +38,4 @@ import{a as I,P}from"./vendor-b592f4c5.js";(function(){const r=document.createEl
           <use href="${l}#icon-heroicons-solid_shopping-cart-18x18"></use>
         </svg>
       `,t.disabled=!1)})}export{E as c,l as i};
-//# sourceMappingURL=products-list-39a0f548.js.map
+//# sourceMappingURL=products-list-8638d7ff.js.map
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{i as p}from"./assets/products-list-39a0f548.js";import{a as l}from"./assets/vendor-b592f4c5.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.querySelector(".footer-sub-form");t.addEventListener("submit",async e=>{e.preventDefault();const s=t.querySelector(".footer-email-input"),o=s.value;if(!s.checkValidity()){alert("Please enter a valid email address.");return}const n={email:o};try{const i=await fetch("https://food-boutique.b.goit.study/api/subscription",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(n)});if(i.ok){const a=await i.json();alert(a.message)}else if(i.status===409)alert("This email is already subscribed.");else throw new Error("Failed to subscribe.")}catch(i){alert("Error: "+i.message)}})});document.addEventListener("DOMContentLoaded",function(){const t=document.getElementById("discount-products");document.getElementById("pagination");async function e(o){try{const i=(await l.get("https://food-boutique.b.goit.study/api/products/discount?page=${page}&limit=${itemsPerPage}`")).data;console.log(i),t.innerHTML="",i.slice(0,2).forEach(d=>{const u=s(d);t.innerHTML+=u})}catch(n){console.error("Error fetching discount products:",n)}}e();function s(o){return`
+import{i as p}from"./assets/products-list-8638d7ff.js";import{a as l}from"./assets/vendor-b592f4c5.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.querySelector(".footer-sub-form");t.addEventListener("submit",async i=>{i.preventDefault();const s=t.querySelector(".footer-email-input"),o=s.value;if(!s.checkValidity()){alert("Please enter a valid email address.");return}const e={email:o};try{const n=await fetch("https://food-boutique.b.goit.study/api/subscription",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(e)});if(n.ok){const a=await n.json();alert(a.message)}else if(n.status===409)alert("This email is already subscribed.");else throw new Error("Failed to subscribe.")}catch(n){alert("Error: "+n.message)}})});document.addEventListener("DOMContentLoaded",function(){const t=document.getElementById("discount-products");document.getElementById("pagination");async function i(o){try{const n=(await l.get("https://food-boutique.b.goit.study/api/products/discount?page=${page}&limit=${itemsPerPage}`")).data;console.log(n),t.innerHTML="",n.slice(0,2).forEach(d=>{const u=s(d);t.innerHTML+=u})}catch(e){console.error("Error fetching discount products:",e)}}i();function s(o){return`
         <li class="card-container-list-discount">
         <div class="photo-card-list-discount">
                 <a class="product-modal-list-discount" href="МОДАЛЬНЕ ВІКНО">
@@ -20,21 +20,21 @@ import{i as p}from"./assets/products-list-39a0f548.js";import{a as l}from"./asse
             </div>
         </li>
     
-        `}});const m="https://food-boutique.b.goit.study/api/products/popular";function h(t){const e=document.querySelector(".popular-product-list");t.forEach(s=>{const o=document.createElement("li");o.classList.add("popular-product-item"),o.innerHTML=`
+        `}});const m="https://food-boutique.b.goit.study/api/products/popular";function h(t){const i=document.querySelector(".popular-product-list");t.forEach(s=>{const o=document.createElement("li");o.classList.add("popular-product-item");const e=s.category.split("_").join(" ");o.innerHTML=`
       <a class="popular-modal">
                  <div class="popular-img">
             <img class="popular-photo-item" src="${s.img}" alt="${s.name}" width="56" height="56" loading="lazy">
           </div>  
           <ul class="about-popular">
             <li class="name-popular-product">${s.name}</li>
-            <li class="subname-popular-product">Category: <span class="id-subname">${s.category}</span></li>
+            <li class="subname-popular-product">Category: <span class="id-subname">${e}</span></li>
             <li class="subname-popular-product">Size: <span class="id-subname">${s.size}</span></li>
             <li class="subname-popular-product">Popularity: <span class="id-subname">${s.popularity}</span></li>
           </ul>
           <button class='popular-cart-btn' type="button">
           <svg class="list-cart-svg-list" width="12" height="12">
-<use href="'${p}'#icon-heroicons-solid_shopping-cart-12x12 "></use>
+<use href="${p}#icon-heroicons-solid_shopping-cart-12x12 "></use>
          </svg></button>
         </a>
-    `,e.appendChild(o)})}async function g(){try{return(await l.get(m)).data}catch(t){throw console.error("Error fetching popular products:",t),t}}g().then(t=>{h(t)}).catch(t=>{console.error("Error:",t)});window.onscroll=()=>r();window.addEventListener("scroll",c(r,250)),window.addEventListener("resize",c(r,250));async function r(){const t=document.querySelector("body"),e=document.body.offsetHeight,s=window.innerHeight,o=window.scrollY,n=e-s/4;o+s>n?t.classList.add("body--no-transparency"):t.classList.remove("body--no-transparency")}function c(t,e){let s=null;return function(...n){s||(s=setTimeout(()=>{t(...n),clearTimeout(s),s=null},e))}}
+    `,i.appendChild(o)})}async function g(){try{return(await l.get(m)).data}catch(t){throw console.error("Error fetching popular products:",t),t}}g().then(t=>{h(t)}).catch(t=>{console.error("Error:",t)});window.onscroll=()=>r();window.addEventListener("scroll",c(r,250)),window.addEventListener("resize",c(r,250));async function r(){const t=document.querySelector("body"),i=document.body.offsetHeight,s=window.innerHeight,o=window.scrollY,e=i-s/4;o+s>e?t.classList.add("body--no-transparency"):t.classList.remove("body--no-transparency")}function c(t,i){let s=null;return function(...e){s||(s=setTimeout(()=>{t(...e),clearTimeout(s),s=null},i))}}
 //# sourceMappingURL=commonHelpers2.js.map
