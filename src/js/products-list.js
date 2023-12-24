12d3b833415e7fe34555c6a3affa587871537d1f
jaycikey <138725941+jaycikey@users.noreply.github.com>
2023-12-24 16:45:32 -0500

    fixIconAndvisiblePagesForMobale

--- src/js/products-list.js
+++ src/js/products-list.js
@@ -1,10 +1,11 @@
 import icons from '../img/icone/symbol-defs.svg';
 import { getProducts } from './products-api';
-import { addToCart, getCart, getFilters, updateFilter, removeFromCart } from './local-storage';
+import { addToCart, getCart, getFilters, updateFilter, removeFromCart} from './local-storage';
 import 'tui-pagination/dist/tui-pagination.css';
 import Pagination from 'tui-pagination';
 
 const productsListContainer = document.getElementById('products-list-container');
+
 let pagination;
 
 // Обробка зміни розміру вікна
@@ -23,20 +24,13 @@ function updatePageSize() {
   const currentFilters = getFilters();
   if (currentFilters.limit !== limit) {
     updateFilter('limit', limit);
-    updateFilter('page', 1); // Повернення на першу сторінку
+    updateFilter('page', 1); // Оновлення сторінки до першої
     renderProducts();
   }
 }
 
-// Встановлення обробників подій на кнопки додавання до кошика
-function setCartButtonEventListeners(arr) {
-  document.querySelectorAll('.cart-btn-list').forEach(button => {
-    button.addEventListener('click', (e) => {
-      const productId = e.currentTarget.dataset.productId;
-      handleCartButtonClick(productId, arr);
-    });
-  });
-}
+
+
 
 // Функція, яка викликається при кліку на кнопку додавання продукту до кошика
 function handleCartButtonClick(productId, arr) {
@@ -79,6 +73,16 @@ function updateCartButtonIcons(arr) {
   });
 }
 
+// Встановлення обробників подій на кнопки додавання до кошика
+function setCartButtonEventListeners(arr) {
+  document.querySelectorAll('.cart-btn-list').forEach(button => {
+    button.addEventListener('click', (e) => {
+      const productId = e.currentTarget.dataset.productId;
+      handleCartButtonClick(productId, arr);
+    });
+  });
+}
+
 function removeAndRecreatePaginationContainer() {
   const container = document.getElementById('tui-pagination-container');
   if (container) {
@@ -91,26 +95,32 @@ function removeAndRecreatePaginationContainer() {
   newContainer.className = 'tui-pagination';
   productsListContainer.after(newContainer); // Додавання після контейнера з продуктами
 }
+
 async function renderProducts() {
-  const filters = getFilters();
-  let page = filters.page || 1;
-  let limit = filters.limit || 6;
+    const filters = getFilters();
+    let page = filters.page || 1;
+    let limit = filters.limit || 6;
 
-  try {
-    const { data } = await getProducts(page, limit);
-    const { perPage, totalPages, results } = data;
-    const totalItems = perPage * totalPages;
+    try {
+        const { data } = await getProducts(page, limit);
+        const { perPage, totalPages, results } = data;
+        const totalItems = perPage * totalPages;
 
-    productsListContainer.innerHTML = createMarkup(results);
-    setCartButtonEventListeners(results);
+       
+    productsListContainer.innerHTML = createMarkup(results); // Оновлюємо тільки список продуктів
+    setCartButtonEventListeners(results); // Встановлення обробників подій на нові кнопки
 
     removeAndRecreatePaginationContainer();
 
     const container = document.getElementById('tui-pagination-container');
+     // Визначення кількості видимих сторінок на основі ширини екрану
+     const visiblePages = window.innerWidth < 768 ? 2 : 4; // За замовчуванням 4, якщо ширина екрану більша за 768
+
+
     pagination = new Pagination(container, {
       totalItems: totalItems,
       itemsPerPage: limit,
-      visiblePages: 4,
+      visiblePages: visiblePages,
       centerAlign: true,
       page: page,
     });
@@ -132,6 +142,8 @@ async function renderProducts() {
   }
 }
 
+
+renderProducts();
 
 export function createMarkup(arr) {
   const markup = `<ul class="card-container-list">${arr
@@ -172,10 +184,19 @@ export function createMarkup(arr) {
     })
     .join('')}</ul>`;
     setTimeout(() => {
-      setCartButtonEventListeners(arr); // Перенесення встановлення обробників подій
+      document.querySelectorAll('.cart-btn-list').forEach(button => {
+        button.addEventListener('click', (e) => {
+          const productId = e.currentTarget.dataset.productId;
+          const product = arr.find(item => item._id === productId);
+          if (product) {
+            handleCartButtonClick(product, arr);
+          } else {
+            console.error('Product not found for ID:', productId);
+          }
+        });
+      });
     }, 0);
 
+    setCartButtonEventListeners(arr); // Встановлення обробників подій після створення розмітки
     return markup;
 }
-
-renderProducts();
