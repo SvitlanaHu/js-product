8123417842d1804f2b3b6a8286ea11c848b16653
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-12-24 03:45:21 +0200

    edit apies

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -2,26 +2,30 @@ import axios from 'axios';
 
 const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
 
-export function getProducts(page, limit) {
+// export function getProducts(page, limit) {
+//     return axios.get(`${BASE_URL}`, {
+//         params: {
+//             page: page,
+//             limit: limit,
+//         },
+//     });
+// };
+
+export function getProductsKeyword(page, limit, value) {
     return axios.get(`${BASE_URL}`, {
         params: {
             page: page,
             limit: limit,
-        },
-    });
-};
-
-export function getProductsKeyword(value) {
-    return axios.get(`${BASE_URL}`, {
-        params: {
             keyword: value,
         },
     });
 };
 
-export function getProductsCategory(category) {
+export function getProductsCategory(page, limit, category) {
     return axios.get(`${BASE_URL}`, {
         params: {
+            page: page,
+            limit: limit,
             category: category,
         },
     });
