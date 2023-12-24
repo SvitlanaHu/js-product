bf638bd249a6ab1ac197a476c64d5483af563499
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-24 03:54:17 +0200

    Update products-api.js

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
 
-// export function getProducts(page, limit) {
-//     return axios.get(`${BASE_URL}`, {
-//         params: {
-//             page: page,
-//             limit: limit,
-//         },
-//     });
-// };
+export function getProducts(page, limit) {
+    return axios.get(`${BASE_URL}`, {
+        params: {
+            page: page,
+            limit: limit,
+        },
+    });
+};
 
 export function getProductsKeyword(page, limit, value) {
     return axios.get(`${BASE_URL}`, {
