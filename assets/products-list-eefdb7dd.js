066eac16469111cce66a4dfd089f5343c4ec1cad
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-24 12:25:07 +0000

    Deploying to gh-pages from @ SvitlanaHu/js-product@0fceed7a9a84e136937536c5e6abb9ae7e01cae2 🚀

---
+++ assets/products-list-eefdb7dd.js
@@ -0,0 +1,41 @@
+import{a as P,P as C}from"./vendor-b592f4c5.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function t(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerpolicy&&(r.referrerPolicy=s.referrerpolicy),s.crossorigin==="use-credentials"?r.credentials="include":s.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(s){if(s.ep)return;s.ep=!0;const r=t(s);fetch(s.href,r)}})();const l="/js-product/assets/symbol-defs-16fdabac.svg",$="https://food-boutique.b.goit.study/api/products";function w(e,i){return P.get(`${$}`,{params:{page:e,limit:i}})}function m(e){localStorage.setItem("productFilters",JSON.stringify(e))}function d(){const e=localStorage.getItem("productFilters");return e?JSON.parse(e):{keyword:null,category:null,page:1,limit:6}}function h(e,i){const t=d();t[e]=i,m(t)}function S(e){localStorage.setItem("shoppingCart",JSON.stringify(e))}function u(){const e=localStorage.getItem("shoppingCart");return e?JSON.parse(e):[]}function L(e){const i=u(),t=i.findIndex(o=>o._id===e._id);t>-1?i[t].quantity+=1:i.push({...e,quantity:1}),S(i)}const _=document.getElementById("products-list-container");let c;async function y(){let e=d(),i=e.page||1,t;window.innerWidth>=1440?t=e.limit=9:window.innerWidth>=768?t=e.limit=8:t=e.limit=6,m(e);try{const{data:o}=await w(i,t),{perPage:s,totalPages:r,results:n}=o,p=s*r;_.innerHTML=E(n);const b=document.getElementById("tui-pagination-container");c?(c.reset(p),c.movePageTo(i)):(c=new C(b,{totalItems:p,itemsPerPage:t,visiblePages:4,centerAlign:!0,page:i}),c.on("beforeMove",I=>{const g=I.page,a=d(),f=a.limit||6;(g!==a.page||f!==a.limit)&&(h("page",g),h("limit",f),y())})),v()}catch(o){console.error("Error fetching products",o)}}y();function E(e){const i=`<ul class="card-container-list">${e.map(t=>{const o=t.category.split("_").join(" ");return`
+            <li class="photo-card-list">
+                <a class="product-modal-list" href="#">
+                    <div class="img-container-list">
+                        <img class="product-image-list" src="${t.img}" alt="${t.name} photo" width=140 height=140 loading="lazy" />
+                    </div>
+                    <div class="product-info-list">
+                        <h2 class="product-name-list">${t.name}</h2>
+                        <div class='product-atributes-list'>
+                            <p class="atributes-info-list">
+                                <b class="atributes-list">Category:</b> ${o}
+                            </p>
+                            <p class="atributes-info-list">
+                                <b class="atributes-list">Size:</b> ${t.size}
+                            </p>
+                            <p class="atributes-info-list">
+                                <b class="atributes-list">Popularity:</b> ${t.popularity}
+                            </p>
+                        </div>
+                        </a>
+                        <div class="price-and-btn-list">
+                            <h2 class="product-price-list">$${t.price}</h2>
+                            <button class='cart-btn-list' type="button" data-product-id="${t._id}">          
+                                <svg class="list-cart-svg-list" width="18" height="18" >
+                                    <use href="${l}#icon-heroicons-solid_shopping-cart-18x18">
+                                    </use>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+            </li>
+        `}).join("")}</ul>`;return setTimeout(()=>{document.querySelectorAll(".cart-btn-list").forEach(t=>{t.addEventListener("click",o=>{const s=o.currentTarget.dataset.productId,r=e.find(n=>n._id===s);r?F(r):console.error("Product not found for ID:",s)})})},0),i}function F(e,i){u().find(s=>s._id===e._id)?console.log("Product is already in the cart"):(L(e),v())}function v(e){const i=u();document.querySelectorAll(".cart-btn-list").forEach(t=>{const o=t.dataset.productId;i.find(r=>r._id===o)?(t.innerHTML=`
+        <svg class="list-cart-svg-list" width="18" height="18">
+          <use href="${l}#icon-check"></use>
+        </svg>
+      `,t.disabled=!0):(t.innerHTML=`
+        <svg class="list-cart-svg-list" width="18" height="18">
+          <use href="${l}#icon-heroicons-solid_shopping-cart-18x18"></use>
+        </svg>
+      `,t.disabled=!1)})}export{E as c,l as i};
+//# sourceMappingURL=products-list-eefdb7dd.js.map
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{i as p}from"./assets/products-list-f1e3c316.js";import{a as l}from"./assets/vendor-b592f4c5.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.querySelector(".footer-sub-form");t.addEventListener("submit",async i=>{i.preventDefault();const s=t.querySelector(".footer-email-input"),o=s.value;if(!s.checkValidity()){alert("Please enter a valid email address.");return}const e={email:o};try{const n=await fetch("https://food-boutique.b.goit.study/api/subscription",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(e)});if(n.ok){const a=await n.json();alert(a.message)}else if(n.status===409)alert("This email is already subscribed.");else throw new Error("Failed to subscribe.")}catch(n){alert("Error: "+n.message)}})});document.addEventListener("DOMContentLoaded",function(){const t=document.getElementById("discount-products");document.getElementById("pagination");async function i(o){try{const n=(await l.get("https://food-boutique.b.goit.study/api/products/discount?page=${page}&limit=${itemsPerPage}`")).data;console.log(n),t.innerHTML="",n.slice(0,2).forEach(d=>{const u=s(d);t.innerHTML+=u})}catch(e){console.error("Error fetching discount products:",e)}}i();function s(o){return`
+import{i as l}from"./assets/products-list-eefdb7dd.js";import{a as d}from"./assets/vendor-b592f4c5.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.querySelector(".footer-sub-form");t.addEventListener("submit",async i=>{i.preventDefault();const s=t.querySelector(".footer-email-input"),o=s.value;if(!s.checkValidity()){alert("Please enter a valid email address.");return}const e={email:o};try{const n=await fetch("https://food-boutique.b.goit.study/api/subscription",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(e)});if(n.ok){const a=await n.json();alert(a.message)}else if(n.status===409)alert("This email is already subscribed.");else throw new Error("Failed to subscribe.")}catch(n){alert("Error: "+n.message)}})});document.addEventListener("DOMContentLoaded",function(){const t=document.getElementById("discount-products");document.getElementById("pagination");async function i(o){try{const n=(await d.get("https://food-boutique.b.goit.study/api/products/discount?page=${page}&limit=${itemsPerPage}`")).data;console.log(n),t.innerHTML="",n.slice(0,2).forEach(u=>{const p=s(u);t.innerHTML+=p})}catch(e){console.error("Error fetching discount products:",e)}}i();function s(o){return`
         <li class="card-container-list-discount">
         <div class="photo-card-list-discount">
                 <a class="product-modal-list-discount" href="МОДАЛЬНЕ ВІКНО">
@@ -11,7 +11,7 @@ import{i as p}from"./assets/products-list-f1e3c316.js";import{a as l}from"./asse
                             <h2 class="price-discount">$${o.price}</h2>
                             <button class='cart-btn-list-discount' type="button">          
                                 <svg class='list-cart-svg-list' width="18" height="18" >
-                                    <use href="./img/icone/symbol-defs.svg#icon-heroicons-solid_shopping-cart-18x18"></use>
+                                    <use href="${l}#icon-heroicons-solid_shopping-cart-18x18"></use>
                                 </svg>
                             </button>
                         </div>
@@ -33,8 +33,8 @@ import{i as p}from"./assets/products-list-f1e3c316.js";import{a as l}from"./asse
           </ul>
           <button class='popular-cart-btn' type="button">
           <svg class="list-cart-svg-list" width="12" height="12">
-<use href="${p}#icon-heroicons-solid_shopping-cart-12x12 "></use>
+<use href="${l}#icon-heroicons-solid_shopping-cart-12x12 "></use>
          </svg></button>
         </a>
-    `,i.appendChild(o)})}async function g(){try{return(await l.get(m)).data}catch(t){throw console.error("Error fetching popular products:",t),t}}g().then(t=>{h(t)}).catch(t=>{console.error("Error:",t)});window.onscroll=()=>r();window.addEventListener("scroll",c(r,250)),window.addEventListener("resize",c(r,250));async function r(){const t=document.querySelector("body"),i=document.body.offsetHeight,s=window.innerHeight,o=window.scrollY,e=i-s/4;o+s>e?t.classList.add("body--no-transparency"):t.classList.remove("body--no-transparency")}function c(t,i){let s=null;return function(...e){s||(s=setTimeout(()=>{t(...e),clearTimeout(s),s=null},i))}}
+    `,i.appendChild(o)})}async function g(){try{return(await d.get(m)).data}catch(t){throw console.error("Error fetching popular products:",t),t}}g().then(t=>{h(t)}).catch(t=>{console.error("Error:",t)});window.onscroll=()=>r();window.addEventListener("scroll",c(r,250)),window.addEventListener("resize",c(r,250));async function r(){const t=document.querySelector("body"),i=document.body.offsetHeight,s=window.innerHeight,o=window.scrollY,e=i-s/4;o+s>e?t.classList.add("body--no-transparency"):t.classList.remove("body--no-transparency")}function c(t,i){let s=null;return function(...e){s||(s=setTimeout(()=>{t(...e),clearTimeout(s),s=null},i))}}
 //# sourceMappingURL=commonHelpers2.js.map
