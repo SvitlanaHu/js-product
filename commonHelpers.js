e588287c5bf9dae031f926b08989fe8fde6da770
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-25 07:40:43 +0000

    Deploying to gh-pages from @ SvitlanaHu/js-product@d6b0ba43cd9216d5b6c8dec11aa084a8ca35ca50 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,32 +1,32 @@
-import{g as n,s as m,i as b,c as g}from"./assets/local-storage-03e6884f.js";const l=document.querySelector(".js-cart-list"),h=document.querySelector(".js-delete-all-btn"),a=document.querySelector(".all-content-wrap"),f=document.querySelector(".js-number-of-products"),v=document.querySelector(".js-cart-amount");document.querySelector(".order-form");C();o();function C(){const t=n();a.classList.add("is-visible-main-content"),S(t),l.addEventListener("click",L)}function L(t){if(!t.target.classList.contains("js-delete-product-btn"))return;const e=t.target.closest(".js-cart-item"),s=e.dataset.id,r=n().filter(({_id:i})=>s!==i);m(r),e.style.display="none",o(),j(),d()}function j(){n().length||a.classList.replace("is-visible-main-content","is-hidden-main-content")}function S(t){if(!t.length){a.classList.add("is-hidden-main-content");return}const e=t.map(({_id:s,name:c,img:r,category:i,price:u,size:p})=>`<li class="cart-item js-cart-item" data-id = ${s}>
+import{g as c,s as p,i as f,c as g}from"./assets/local-storage-03e6884f.js";const r=document.querySelector(".js-cart-list"),b=document.querySelector(".js-delete-all-btn"),i=document.querySelector(".all-content-wrap"),h=document.querySelector(".js-number-of-products"),v=document.querySelector(".js-cart-amount"),L=document.querySelector(".order-form");C();r.addEventListener("click",S);o();function C(){const t=c();i.classList.add("is-visible-main-content"),y(t),L.addEventListener("submit",x)}function S(t){if(!t.target.classList.contains("js-delete-product-btn"))return;const e=t.target.closest(".js-cart-item"),s=e.dataset.id,a=c().filter(({_id:l})=>s!==l);p(a),e.style.display="none",o(),j(),d()}function j(){c().length||(i.classList.replace("is-visible-main-content","is-hidden-main-content"),r.innerHTML="")}function y(t){if(!t.length){i.classList.add("is-hidden-main-content");return}const e=t.map(({_id:s,name:n,img:a,category:l,price:u,size:m})=>`<li class="cart-item js-cart-item" data-id = ${s}>
        <span class="delete-product-btn js-delete-product-btn">
               <svg width="12" height="12" class='js-delete-product-btn'>
-                <use class='js-delete-product-btn' href="${b}#close-button"></use></svg
+                <use class='js-delete-product-btn' href="${f}#close-button"></use></svg
             ></span>
           <div class="cart-item-info-wrap">
             <div class="cart-img-wrap-bgc">
               <div class="cart-img-thumb">
               <img
-                src="${r}"
-                alt="${c}"
+                src="${a}"
+                alt="${n}"
                 width="100"
                 height="100"
               />
               </div>
             </div>
             <div class="cart-item-descr">
-              <h3 class="cart-item-title">${c}</h3>
+              <h3 class="cart-item-title">${n}</h3>
               <div class="cart-text-wrap">
                 <p class="cart-item-text">
                   <span class="cart-light-text">Category:</span
-                  >&nbsp;&nbsp;${i}
+                  >&nbsp;&nbsp;${l}
                 </p>
                 <p class="cart-item-text">
-                  <span class="cart-light-text">Size:</span>&nbsp;&nbsp;${p}
+                  <span class="cart-light-text">Size:</span>&nbsp;&nbsp;${m}
                 </p>
               </div>
               <p class="cart-item-price">${u}</p>
             </div>
           </div>
-        </li>`).join("");return l.innerHTML=e}h.addEventListener("click",()=>{g(),o(),a.classList.replace("is-visible-main-content","is-hidden-main-content")});function o(){const t=n();f.textContent=t.length}function d(){const e=n().reduce((s,{price:c})=>s+=c,0);v.textContent=e}d();
+        </li>`).join("");return r.innerHTML=e}b.addEventListener("click",()=>{g(),r.innerHTML="",o(),i.classList.replace("is-visible-main-content","is-hidden-main-content")});function o(){const t=c();h.textContent=t.length}function d(){const e=c().reduce((s,{price:n,quantity:a})=>s+=n*a,0);v.textContent=e}d();function x(t){t.preventDefault();const e=t.currentTarget.elements.email.value;if(!e){alert("Please, write your email!");return}$(e),t.currentTarget.reset()}function $(t){if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(t)){alert("Please, enter valid Email!");return}}
 //# sourceMappingURL=commonHelpers.js.map
