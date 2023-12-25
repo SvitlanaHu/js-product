d6c06ba3c34eea493b24e952bf411d529869950c
vladvolchek <mskoresh19@gmail.com>
2023-12-25 23:51:04 +0200

    Header

--- src/js/header.js
+++ src/js/header.js
@@ -2,8 +2,7 @@ import { addToCart } from "./local-storage";
 import { getCart } from "./local-storage";
 
 const headerCart = document.querySelector('.cart-btn')
-const allContentWrap = document.querySelector('.all-content-wrap');
-const numberOfProducts = document.querySelector('.js-number-of-products');
+const numberOfProducts = document.querySelector('.numbers-of-products');
 const cartAmount = document.querySelector('.js-cart-amount');
 const orderForm = document.querySelector('.order-form');
 
@@ -13,3 +12,4 @@ function getNumberOfProducts() {
   numberOfProducts.textContent = lsData.length;
 }
 
+getNumberOfProducts()
