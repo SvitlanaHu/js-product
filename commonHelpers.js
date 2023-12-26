45c4c954ed8dd433ec4545b0939b335df577b267
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-26 19:11:07 +0000

    Deploying to gh-pages from @ SvitlanaHu/js-product@a711a68f2746f1e6b7fc21e61eef5b0271f2e521 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,32 +1,32 @@
-import{g as c,s as p,i as m,c as f}from"./assets/symbol-defs-d40a96bb.js";const e={cartProductList:document.querySelector(".js-cart-list"),deleteAllBtn:document.querySelector(".js-delete-all-btn"),emptyBasketContent:document.querySelector(".empty-basket-content"),emptyBasketWrap:document.querySelector(".empty-basket-wrap"),cartMainContainer:document.querySelector(".main-content-with-delete-all"),numberOfProducts:document.querySelector(".js-number-of-products"),cartTotalPrice:document.querySelector(".js-cart-amount"),orderForm:document.querySelector(".order-form")};y();l();e.cartProductList.addEventListener("click",h);function y(){const t=c();if(!t.length){e.emptyBasketContent.hidden=!1;return}g(t),e.cartMainContainer.hidden=!1,e.emptyBasketWrap.style.display="none",o(),e.orderForm.addEventListener("submit",k)}function l(){const t=c();e.numberOfProducts.textContent=t.length}function h(t){if(!t.target.classList.contains("js-delete-product-btn"))return;const r=t.target.closest(".js-cart-item"),a=r.dataset.id,s=c().filter(({_id:i})=>a!==i);p(s),r.style.display="none",l(),b(),o()}function b(){c().length||(e.cartMainContainer.hidden=!0,e.emptyBasketContent.hidden=!1,e.emptyBasketWrap.style.display="block",e.cartProductList.innerHTML="")}function g(t){const r=t.map(({_id:a,name:n,img:s,category:i,price:d,size:u})=>`<li class="cart-item js-cart-item" data-id = ${a}>
+import{g as o,s as f,i as y,c as u,u as h}from"./assets/symbol-defs-f7ff4350.js";const e={cartProductList:document.querySelector(".js-cart-list"),deleteAllBtn:document.querySelector(".js-delete-all-btn"),emptyBasketContent:document.querySelector(".empty-basket-content"),emptyBasketWrap:document.querySelector(".empty-basket-wrap"),cartMainContainer:document.querySelector(".main-content-with-delete-all"),numberOfProducts:document.querySelector(".js-number-of-products"),cartTotalPrice:document.querySelector(".js-cart-amount"),orderForm:document.querySelector(".order-form")};g();l();e.cartProductList.addEventListener("click",b);function g(){const t=o();if(!t.length){e.emptyBasketContent.hidden=!1;return}k(t),e.cartMainContainer.hidden=!1,e.emptyBasketWrap.style.display="none",d(),e.orderForm.addEventListener("submit",P)}function l(){const t=o();e.numberOfProducts.textContent=t.length}function b(t){if(!t.target.classList.contains("js-delete-product-btn"))return;const a=t.target.closest(".js-cart-item"),c=a.dataset.id,n=o().filter(({_id:r})=>c!==r);f(n),a.style.display="none",l(),C(),d()}function C(){o().length||(e.cartMainContainer.hidden=!0,e.emptyBasketContent.hidden=!1,e.emptyBasketWrap.style.display="block",e.cartProductList.innerHTML="")}function k(t){const a=t.map(({_id:c,name:s,img:n,category:r,price:i,size:m})=>`<li class="cart-item js-cart-item" data-id = ${c}>
        <span class="delete-product-btn js-delete-product-btn">
               <svg width="12" height="12" class='js-delete-product-btn'>
-                <use class='js-delete-product-btn' href="${m}#close-button"></use></svg
+                <use class='js-delete-product-btn' href="${y}#close-button"></use></svg
             ></span>
           <div class="cart-item-info-wrap">
             <div class="cart-img-wrap-bgc">
               <div class="cart-img-thumb">
               <img
-                src="${s}"
-                alt="${n}"
+                src="${n}"
+                alt="${s}"
                 width="100"
                 height="100"
               />
               </div>
             </div>
             <div class="cart-item-descr">
-              <h3 class="cart-item-title">${n}</h3>
+              <h3 class="cart-item-title">${s}</h3>
               <div class="cart-text-wrap">
                 <p class="cart-item-text">
                   <span class="cart-light-text">Category:</span
-                  >&nbsp;&nbsp;${i}
+                  >&nbsp;&nbsp;${r}
                 </p>
                 <p class="cart-item-text">
-                  <span class="cart-light-text">Size:</span>&nbsp;&nbsp;${u}
+                  <span class="cart-light-text">Size:</span>&nbsp;&nbsp;${m}
                 </p>
               </div>
-              <p class="cart-item-price">${d}</p>
+              <p class="cart-item-price">${i}</p>
             </div>
           </div>
-        </li>`).join("");return e.cartProductList.innerHTML=r}e.deleteAllBtn.addEventListener("click",()=>{e.cartProductList.innerHTML="",f(),o(),l(),e.cartMainContainer.hidden=!0,e.emptyBasketContent.hidden=!1,e.emptyBasketWrap.style.display="block"});function o(){const r=c().reduce((a,{price:n,quantity:s})=>a+=n*s,0);e.cartTotalPrice.textContent=r.toFixed(2)}function k(t){t.preventDefault();const r=t.currentTarget.elements.email.value;if(!r){alert("Please, write your email!");return}C(r),t.currentTarget.reset()}function C(t){if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(t)){alert("Please, enter valid Email!");return}}
+        </li>`).join("");return e.cartProductList.innerHTML=a}e.deleteAllBtn.addEventListener("click",p);function p(){e.cartProductList.innerHTML="",u(),d(),l(),e.cartMainContainer.hidden=!0,e.emptyBasketContent.hidden=!1,e.emptyBasketWrap.style.display="block"}function d(){const a=o().reduce((c,{price:s,quantity:n})=>c+=s*n,0);e.cartTotalPrice.textContent=a.toFixed(2)}async function P(t){t.preventDefault();const a=t.currentTarget.elements.email.value;if(!a){alert("Please, write your email!");return}v(a),t.currentTarget.reset();const s=o().map(({_id:r})=>({productId:r,amount:1})),n={email:a,products:s};try{const r=await fetch("https://food-boutique.b.goit.study/api/orders",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(n)});if(r.ok){const i=await r.json();alert(i.message),u(),p(),h()}else if(r.status===400){const i=await r.json();alert("Bad request: "+i.message)}else if(r.status===404)alert("Resource not found.");else if(r.status===500)alert("Server error. Please try again later.");else throw new Error("Failed to subscribe.")}catch(r){alert("Error: "+r.message)}}function v(t){if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(t)){alert("Please, enter valid Email!");return}}
 //# sourceMappingURL=commonHelpers.js.map
