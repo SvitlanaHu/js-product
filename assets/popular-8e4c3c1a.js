cedb30a0fa27088d3b35ea6d5ea5e0ca8aff5620
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-26 23:03:44 +0000

    Deploying to gh-pages from @ SvitlanaHu/js-product@551e481bf63435480ba977fa9630a63d2bccb72e 🚀

--- assets/popular-8e4c3c1a.js
+++ assets/popular-8e4c3c1a.js
@@ -21,4 +21,4 @@ import{a as f}from"./vendor-b592f4c5.js";(function(){const e=document.createElem
                             </button>
         </a>
     `,e.appendChild(n)}),l(t,".popular-cart-btn",u),I(t,".popular-cart-btn",u)}async function L(){try{return(await f.get(v)).data}catch(t){throw console.error("Error fetching popular products:",t),t}}L().then(t=>{S(t)}).catch(t=>{console.error("Error:",t)});export{l as a,I as b,x as c,m as d,q as e,i as g,u as i,d as s,p as u};
-//# sourceMappingURL=popular-0879b870.js.map
+//# sourceMappingURL=popular-8e4c3c1a.js.map
