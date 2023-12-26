7625ac3e4f58c7ce84fffdc56e4a5b170be6dadd
MartaBahrynivska <martabagrynivska@googlemail.com>
2023-12-26 18:54:16 -0500

    scroll up

--- src/js/header.js
+++ src/js/header.js
@@ -1,2 +1,11 @@
 import { updateCartCount } from "./local-storage";
 updateCartCount()
+
+// Get the element
+let topBtn = document.querySelector(".top-btn");
+
+// On Click, Scroll to the page's top, replace 'smooth' with 'instant' if you don't want smooth scrolling
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+// On scroll, Show/Hide the btn with animation
+window.onscroll = () => topBtn.style.opacity = window.scrollY > 500 ? 1 : 0;
