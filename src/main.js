58a66df666d430ff162943c17135985045354e9e
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-12-27 02:10:52 +0200

    edit header count

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import './js/products-api';
 import './js/footer';
-
 import './js/loader';
 import './js/discount';
 import './js/header';
 import './js/local-storage';
-import './js/hero';
 import './js/popular';
 import "./js/filters";
 import './js/products-list';
