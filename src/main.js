b24f05faa9d8a161ac8c96fdb0d7af29e976b535
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-12-27 02:10:42 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import './js/loader';
 import './js/discount';
 import './js/header';
 import './js/local-storage';
-import './js/hero';
 import './js/popular';
 import "./js/filters";
 import './js/products-list';
