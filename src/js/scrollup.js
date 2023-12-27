041ac67ac34229b86794c1769ac59f657d204e76
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-12-28 00:06:49 +0200

    scroll up edit js and css

--- src/js/scrollup.js
+++ src/js/scrollup.js
@@ -3,5 +3,4 @@ let topBtn = document.querySelector(".top-btn");
 
 topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
-
-window.onscroll = () => topBtn.style.opacity = window.scrollY > 500 ? 1 : 0;
+window.onscroll = () => topBtn.style.opacity = window.scrollY > '500' ? '1' : '0';
