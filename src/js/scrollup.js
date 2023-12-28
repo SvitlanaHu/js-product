32070db40d7206546315866bf95ae38df91731e7
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-12-28 14:07:53 +0200

    add new scroll up

--- src/js/scrollup.js
+++ src/js/scrollup.js
@@ -1,11 +1,34 @@
-let topBtn = document.querySelector(".top-btn");
+const offset = 100;
+const scrollUp = document.querySelector('.scroll-up');
+const scrollUpSvgPath = document.querySelector('.scroll-up__svg-path');
+const pathLength = scrollUpSvgPath.getTotalLength();
 
-topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
 
-window.onscroll = () => {
-    if (window.scrollY > 500) {
-        topBtn.style.display = "flex";
+const getTop = () => {
+    return window.pageYOffset || document.documentElement.scrollTop;
+}
+
+const updateDashoffset = () => {
+    const height = document.documentElement.scrollHeight - window.innerHeight;
+    const dashoffset = pathLength - (getTop() * pathLength / height);
+    scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+}
+
+window.addEventListener('scroll', () => {
+    updateDashoffset();
+
+    if (getTop() > offset) {
+        scrollUp.classList.add('scroll-up--active');
     } else {
-        topBtn.style.display = "none";
+        scrollUp.classList.remove('scroll-up--active');
     }
-};
+});
+
+scrollUp.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
